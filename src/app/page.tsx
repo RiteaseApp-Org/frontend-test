ea8a4485f55d6de998f03be18d6d1fa720195863
Chilaka Michael Obinna <obichilaka@gmail.com>
2025-03-18 23:56:15 +0100

    setup of pdf.js worker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react';
+import { useDropzone } from 'react-dropzone';
+import {Document, Page, pdfjs} from "react-pdf";
+import { PDFDocument, rgb } from 'pdf-lib';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const DocumentUploader = () => {
   return (
