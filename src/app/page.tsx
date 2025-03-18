c350193145cde32eaa8778e6f1df9d1a6ac223bb
Chilaka Michael Obinna <obichilaka@gmail.com>
2025-03-19 00:11:04 +0100

    adding annotations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import React, {useState} from 'react';
-import { useDropzone } from 'react-dropzone';
-import {Document, Page, pdfjs} from "react-pdf";
-import { PDFDocument, rgb } from 'pdf-lib';
+import React, { useState } from "react";
+import { useDropzone } from "react-dropzone";
+import { Document, Page, pdfjs } from "react-pdf";
+import { PDFDocument, rgb } from "pdf-lib";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
@@ -12,16 +12,37 @@ const DocumentUploader = () => {
   const [annotations, setAnnotations] = useState([]);
   const [signatures, setSignatures] = useState([]);
 
-  const {getRootProps, getInputProps} = useDropzone({
+  const { getRootProps, getInputProps } = useDropzone({
     accept: "application/pdf",
     onDrop: (acceptedFiles) => {
       setFile(acceptedFiles[0]);
+    },
+  });
+
+  const addHighlight = () => {
+    setAnnotations([...annotations, { type: "highlight", color: "yellow" }]);
+  };
+
+  const addUnderline = () => {
+    setAnnotations([...annotations, { type: "underline", color: "red" }]);
+  };
+
+  const addComment = () => {
+    const comment = prompt("Enter your comment:");
+    if (comment) {
+      setAnnotations([...annotations, { type: "comment", text: comment }]);
     }
-  })
+  };
 
-  return (
-    <div>Document Uploader</div>
-  )
-}
+  const addSignature = () => {
+    const signature = prompt("Enter your signature:");
+    if (signature) {
+      setSignatures([...signatures, { text: signature }]);
+    }
+  };
 
-export default DocumentUploader
+
+  return <div>Document Uploader</div>;
+};
+
+export default DocumentUploader;
